e307945edbf7e4dfde9c0861d0a637d57ff6f59b
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2023-02-06 14:11:33 +0100

    Upgrade to PHP 7.4, upgrade packages, fix crypto

--- src/CryptoHelper.php
+++ src/CryptoHelper.php
@@ -4,13 +4,19 @@ namespace WOM;
 class CryptoHelper {
 
     public static function LoadPublicKeyFromString($key) {
-        $rsa = new \phpseclib\Crypt\RSA();
-        if(!$rsa->loadKey($key)){
+        $key = \phpseclib3\Crypt\PublicKeyLoader::load($key);
+        if(!$key){
             \WOM\Logger::$Instance->error("Public key is invalid");
             throw new \InvalidArgumentException("Public key is invalid");
         }
+        if(!$key instanceof \phpseclib3\Crypt\Common\PublicKey) {
+            \WOM\Logger::$Instance->error("Loaded key if not public");
+            throw new \InvalidArgumentException("Key is not public");
+        }
 
-        return $rsa;
+        \WOM\Logger::$Instance->debug("Public key loaded successfully from string");
+
+        return $key;
     }
 
     public static function LoadPublicKeyFromPath($keyPath) {
@@ -19,13 +25,19 @@ class CryptoHelper {
             throw new \InvalidArgumentException("{$keyPath} public key file does not exist");
         }
 
-        $rsa = new \phpseclib\Crypt\RSA();
-        if(!$rsa->loadKey(file_get_contents($keyPath))){
+        $key = \phpseclib3\Crypt\PublicKeyLoader::load(file_get_contents($keyPath));
+        if(!$key){
             \WOM\Logger::$Instance->error("{$keyPath} public key file is invalid");
             throw new \InvalidArgumentException("{$keyPath} public key file is invalid");
         }
+        if(!$key instanceof \phpseclib3\Crypt\Common\PublicKey) {
+            \WOM\Logger::$Instance->error("Loaded key if not public");
+            throw new \InvalidArgumentException("Key is not public");
+        }
 
-        return $rsa;
+        \WOM\Logger::$Instance->debug("Public key loaded successfully from path {$keyPath}");
+
+        return $key;
     }
 
     public static function LoadPrivateKey($keyPath, $passphrase = null, $logger = null) {
@@ -34,28 +46,32 @@ class CryptoHelper {
             throw new \InvalidArgumentException("{$keyPath} private key file does not exist");
         }
 
-        // load private key
-        $rsa = new \phpseclib\Crypt\RSA();
-        if($passphrase != ''){
-            $rsa->setPassword($passphrase);
-        }
-        if(!$rsa->loadKey(file_get_contents($keyPath))){
+        $key = \phpseclib3\Crypt\PublicKeyLoader::load(file_get_contents($keyPath), $password = $passphrase);
+        if(!$key){
             \WOM\Logger::$Instance->error("{$keyPath} private key file is invalid");
             throw new \InvalidArgumentException("{$keyPath} private key file is invalid");
         }
+        if(!$key instanceof \phpseclib3\Crypt\Common\PrivateKey) {
+            \WOM\Logger::$Instance->error("Loaded key if not private");
+            throw new \InvalidArgumentException("Key is not private");
+        }
 
-        return $rsa;
+        \WOM\Logger::$Instance->debug("Private key loaded successfully from path {$keyPath}");
+
+        return $key;
     }
 
     public static function Encrypt($payload, $key){
-        $key->setEncryptionMode(\phpseclib\Crypt\RSA::ENCRYPTION_PKCS1);
+        $key = $key->withPadding(\phpseclib3\Crypt\RSA::ENCRYPTION_PKCS1);
 
+        \WOM\Logger::$Instance->debug("Encrypting payload of " . mb_strlen($payload) . " characters");
         return $key->encrypt($payload);
     }
 
     public static function Decrypt($payload, $key){
-        $key->setEncryptionMode(\phpseclib\Crypt\RSA::ENCRYPTION_PKCS1);
+        $key = $key->withPadding(\phpseclib3\Crypt\RSA::ENCRYPTION_PKCS1);
 
+        \WOM\Logger::$Instance->debug("Decrypting payload of " . mb_strlen($payload) . " characters");
         return $key->decrypt(base64_decode($payload));
     }
 
